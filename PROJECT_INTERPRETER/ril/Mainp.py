00f47699a20c23a0b3ca0f27d2b978e800b9c8bd
Tania <89388944+nathaniasan@users.noreply.github.com>
2022-10-21 20:44:21 +0700

    Update Mainp.py
    Signed-off-by: Tania <89388944+nathaniasan@users.noreply.github.com>

--- PROJECT_INTERPRETER/ril/Mainp.py
+++ PROJECT_INTERPRETER/ril/Mainp.py
@@ -5,47 +5,82 @@ from tkinter import BROWSE
 import Class as v
 
 menu = ["[1] Paket Normal"," [2] Login"," [3] exit"]
-
+menu1 = ["[1] Paket untuk Member"," [2] Login"," [3] exit"]
 
 class Warnet : 
     jls_extract_var = menu
+    menumember = menu1
 
     while True:
         v.welcome(" Selamat Datang Di Warnet PI","1 jam = 5000          2 jam = 9000         3 jam = 13000",60)
         v.menu(" SILAKAN PILIH MENU ", jls_extract_var, 60)
         #input 1,2,3
-        number= eval(input("\nMasukkan pilihan :"))
+        while True:
+            try:
+                number= eval(input("\nMasukkan pilihan :"))
+            except (SyntaxError,NameError):
+                print("pilihan invalid")
+                continue
+            break
         if number == 1 :
-            print("1 jam = 5000          2 jam = 9000         3 jam = 13000")
-            pilihan=eval(input("\nPilihan paket :"))
+            print("1 jam = 5000          2 jam = 9000         3 jam = 13000") 
+            while True:
+                try:
+                    pilihan=eval(input("\nPilihan paket :"))
+                except (SyntaxError,NameError):
+                    print("pilihan invalid")
+                    continue
+                break
             if pilihan==1:
                 v.pembayaran(pilihan)
             elif pilihan==2:
                 v.pembayaran(pilihan)
             elif pilihan==3:
                 v.pembayaran(pilihan)
+            else:
+                print("\nPilihan invalid......!\n")
+                continue
         elif number == 2:
             v.login(60)
-            v.menu(" SILAKAN PILIH MENU ", jls_extract_var, 60)
-            print("1 jam = 4000          2 jam = 7000         3 jam = 10000")
-            p=eval(input("\nPilihan paket :"))
-            if p==1:
-                v.pembayaranMember(p)
-            elif p==2:
-                v.pembayaranMember(p)
-            elif p==3:
-                v.pembayaranMember(p)
+            v.menu(" SILAKAN PILIH PAKET SEBAGAI MEMBER",menumember, 60)
+            while True:
+                try:
+                    number= eval(input("\nMasukkan pilihan :"))
+                except (SyntaxError,NameError):
+                    print("pilihan invalid")
+                    continue
+                break
+            if number == 1 :
+                print("1 jam = 4000          2 jam = 7000         3 jam = 10000") 
+                while True:
+                    try:
+                        p=eval(input("\nPilihan paket :"))
+                    except (SyntaxError,NameError):
+                        print("pilihan invalid")
+                        continue
+                    break
+                if p==1:
+                    v.pembayaranMember(p)
+                elif p==2:
+                    v.pembayaranMember(p)
+                elif p==3:
+                    v.pembayaranMember(p)
+                else:
+                    print("\nPilihan invalid......!\n")
+                    continue
 
         elif number == 3:
             v.exit(60)
-            break;
-        while True:
-            try :
-                exit=eval(input("\nKlik 1 untuk melanjutkan :"))        
-            except (NameError, SyntaxError):
-                print ("yang anda masukkan bukan angka")
-                continue
             break
-    v.exit(60)
-            
+        else:
+            print("\nPilihan invalid......!\n")
+            continue
+        try:
+            exit=eval(input("\nKlik 1 untuk melanjutkan :"))    
+        except(SyntaxError,NameError):
+            v.exit(60)
+            break
+        if exit!=1:
+            v.exit(60)
+            break
 Warnet()
