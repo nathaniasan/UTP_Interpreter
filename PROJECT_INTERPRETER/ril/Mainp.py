88d8c838b9e186c4a50d7a6d74b6d02c5a44b709
Tania <89388944+nathaniasan@users.noreply.github.com>
2022-10-21 09:48:16 +0700

    Update Mainp.py

--- PROJECT_INTERPRETER/ril/Mainp.py
+++ PROJECT_INTERPRETER/ril/Mainp.py
@@ -2,19 +2,24 @@ import os
 
 import Class as v
 
-menu = ["[1] Login"," [2] Logout"," [3] Warnet Tutup"]
+menu = ["[1] Paket Normal"," [2] Login"," [3] exit"]
 
 
 class Warnet : 
     jls_extract_var = menu
+
+    v.welcome(" Selamat Datang Di Warnet PI","1 jam = 5000          2 jam = 9000         3 jam = 13000",60)
     v.menu(" SILAKAN PILIH MENU ", jls_extract_var, 60)
+    #input 1,2,3
     number= eval(input("\nMasukkan pilihan :"))
     if number == 1 :
-        v.welcome("Ingin merental berapa jam?",60)
-        jam = eval(input())
-    
-
-    
-
-    
-warnet = Warnet()
+        print("1 jam = 5000          2 jam = 9000         3 jam = 13000")
+        pilihan=eval(input("\nPilihan paket :"))
+        if pilihan==1:
+            v.pembayaran(pilihan)
+        elif pilihan==2:
+            v.pembayaran(pilihan)
+        elif pilihan==3:
+            v.pembayaran(pilihan)
+    if number == 2:
+        v.headerlogin(60)
